55541eeb5a512b228da55b0cebed748a7ef141ad
Ariel Navarro <a.t.navarro.1993@gmail.com>
2021-11-30 17:40:47 -0300

    hotfix

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, AfterViewInit, Input } from '@angular/core';
-import { ProductsService } from 'src/app/core/services/products/products.service';
-import { Swiper } from 'swiper/bundle';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {ProductsService} from 'src/app/core/services/products/products.service';
+import {Swiper} from 'swiper/bundle';
 
 @Component({
   selector: 'app-home',
@@ -11,30 +11,19 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
   mySwiper: Swiper;
   products;
-  test = []
-  constructor(private ps: ProductsService) { }
+  test = [];
 
-
-  public test2(): void{
+  constructor(private ps: ProductsService) {
   }
+
 
   ngOnInit() {
     this.products = this.ps.getAllProducts();
-    this.test.push(this.products[0])
+    this.test.push(this.products[0]);
   }
-  public test(): void{
-    const array = [1,
-      2,
-      3,
-      4,
-      5,
-      6,
-      7,
-      8,
-      9];
-  }
-  ngAfterViewInit(){
-    this.mySwiper = new Swiper ('.swiper-container', {
+
+  ngAfterViewInit() {
+    this.mySwiper = new Swiper('.swiper-container', {
       loop: true,
       pagination: {
         el: '.swiper-pagination',
